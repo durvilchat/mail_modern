c3af3336c2bacb43313d807754c9384b64e2ab71
durvilchat <rostapig@gmail.com>
2018-05-05 09:17:04 +0100

    init

---
+++ mail_modern/static/src/js/thread.js
@@ -0,0 +1,61 @@
+odoo.define('mail_modern.thread', function (require) {
+    "use strict";
+    var core = require('web.core'); 
+    var thread = require('mail.ChatThread');
+
+    var
+        _t = core._t;
+
+
+
+    thread.include({
+
+
+        events: {
+            "click a": "on_click_redirect",
+            "click img": "on_click_redirect",
+            "click strong": "on_click_redirect",
+            "click .o_thread_show_more": "on_click_show_more",
+            "click .o_thread_message_needaction": function (event) {
+                var message_id = $(event.currentTarget).data('message-id');
+                this.trigger("mark_as_read", message_id);
+            },
+            "click .o_thread_message_star": function (event) {
+                var message_id = $(event.currentTarget).data('message-id');
+                this.trigger("toggle_star_status", message_id);
+            },
+            "click .o_thread_message_reply": function (event) {
+                this.selected_id = $(event.currentTarget).data('message-id');
+                this.$('.o_thread_message').removeClass('o_thread_selected_message');
+                this.$('.o_thread_message[data-message-id="' + this.selected_id + '"]')
+                    .addClass('o_thread_selected_message');
+                this.trigger('select_message', this.selected_id);
+                event.stopPropagation();
+            },  "click .coll .o_thread_message_core": function (event) {
+                event.preventDefault();
+                var itemSelect = $(event.currentTarget).find(".o_thread_message_sidebar_small");
+                if (itemSelect.hasClass("hide"))
+                    itemSelect.removeClass('hide');
+                else
+                    itemSelect.addClass('hide');
+            },
+            "click .oe_mail_expand": function (event) {
+                event.preventDefault();
+                var $message = $(event.currentTarget).parents('.o_thread_message');
+                $message.addClass('o_message_expanded');
+                this.expanded_msg_ids.push($message.data('message-id'));
+            },
+            "click .o_thread_message": function (event) {
+                $(event.currentTarget).toggleClass('o_thread_selected_message');
+            },
+            "click": function () {
+                if (this.selected_id) {
+                    this.unselect();
+                    this.trigger('unselect_message');
+                }
+            },
+        },
+
+    });
+
+});
